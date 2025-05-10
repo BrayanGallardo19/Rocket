739d17b4f036bb2cdc1298a14438f845b402fd98
carlos31255 <167790200+carlos31255@users.noreply.github.com>
2025-05-09 20:34:43 -0400

    Add files via upload

---
+++ Gestion de Roles y Permisos/Gestion-de-Roles-y-Permisos/src/main/java/com/example/Gestion/de/Roles/y/Permisos/RolesYPermisosApplication.java
@@ -0,0 +1,13 @@
+package com.example.Gestion.de.Roles.y.Permisos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RolesYPermisosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RolesYPermisosApplication.class, args);
+	}
+
+}
---
+++ Gestion de Roles y Permisos/Gestion-de-Roles-y-Permisos/src/test/java/com/example/Gestion/de/Roles/y/Permisos/RolesYPermisosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Gestion.de.Roles.y.Permisos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RolesYPermisosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
