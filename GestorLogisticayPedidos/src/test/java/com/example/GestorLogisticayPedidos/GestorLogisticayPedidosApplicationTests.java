68f8d937a45b433b2a10c84aadf346482ca96357
carlos31255 <167790200+carlos31255@users.noreply.github.com>
2025-05-15 17:28:36 -0400

    Add files via upload

---
+++ GestorLogisticayPedidos/src/test/java/com/example/GestorLogisticayPedidos/GestorLogisticayPedidosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GestorLogisticayPedidos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestorLogisticayPedidosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
