5883d7cc05b3494bffb9eb35d990b37959cf4a9e
BrunoMirandaR <Br.mirandar@duocuc.cl>
2025-05-16 19:24:31 -0400

    Delete Gestion de Roles y Permisos/Gestion-de-Roles-y-Permisos directory
    Eliminación del micro servicio por conflictos entre carpetas.

--- Gestion de Roles y Permisos/Gestion-de-Roles-y-Permisos/src/main/java/com/example/Gestion/de/repository/PermisoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.Gestion.de.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.Gestion.de.model.Permiso;
-
-public interface PermisoRepository extends JpaRepository<PermisoRepository, Integer> {
-
-}
