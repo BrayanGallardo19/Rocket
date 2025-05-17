a786d68f9f56e5eb05bc961c16f6bf6169dff131
BrunoMirandaR <Br.mirandar@duocuc.cl>
2025-05-16 22:54:14 -0400

    Testeo de gets por Id y nombres funcionales. Siguiente: averiguar como asignar los permisos a los roles.

---
+++ RolesyPermisos/src/main/java/com/example/RolesyPermisos/repository/RoleRepository.java
@@ -0,0 +1,16 @@
+package com.example.RolesyPermisos.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.RolesyPermisos.model.Role;
+
+@Repository
+
+public interface RoleRepository extends JpaRepository<Role, Integer> {
+   
+    List<Role> findByNombre(String nombre);
+    boolean existsByNombre(String nombre);
+}
