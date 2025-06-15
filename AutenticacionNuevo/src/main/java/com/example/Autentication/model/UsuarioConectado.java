78c842757d1bd9e02c1b0b4131d046fd71570436
BrayanGallardo19 <bray.gallardo@duocuc.cl>
2025-06-15 10:21:40 -0400

    --add
    Agregado a microservicio autenticación para que se cree una tabla llamada usuarios_conectados en la cual cada vez que alguien logee guadará el id del usuario, el username, y el token de login (recordar que el token de login es diferente al de registro)

---
+++ AutenticacionNuevo/src/main/java/com/example/Autentication/model/UsuarioConectado.java
@@ -0,0 +1,29 @@
+package com.example.Autentication.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "usuario_conectado")
+public class UsuarioConectado {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false)
+    private Integer userId;
+
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String token;
+}
